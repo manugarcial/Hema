be3778fe7f97e091c90e79a57573dd3bddb50f95
manugarcial <manuel.garcia.developer@gmail.com>
2024-11-23 21:35:19 +0100

    Adding first menu

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,72 @@
+<script>
+  // You can add any reactive properties or functions here
+  let activeLink = '/';
+</script>
+
+<nav>
+  <ul class="navbar">
+    <li class={activeLink === '/home' ? 'active' : ''}>
+      <a href="/" on:click={() => activeLink = '/'}>
+        Tote Bags
+      </a>
+    </li>
+    <li class={activeLink === '/about' ? 'active' : ''}>
+      <a href="/" on:click={() => activeLink = '/about'}>
+        Bolsos
+      </a>
+    </li>
+    <a href="/" class="logo">
+      <img src="/src/static/hema-logo.png" alt="Logo" class="navbar-logo" />
+    </a>
+    <li class={activeLink === '/services' ? 'active' : ''}>
+      <a href="/" on:click={() => activeLink = '/services'}>
+        Lazos
+      </a>
+    </li>
+    <li class={activeLink === '/contact' ? 'active' : ''}>
+      <a href="/" on:click={() => activeLink = '/contact'}>
+        Fundas libros
+      </a>
+    </li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    background-color: #a87866;
+    padding: 10px;
+  }
+
+  .navbar {
+    display: flex;
+    justify-content: space-around;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    align-items: center;
+  }
+
+  .navbar li {
+    margin: 0 15px;
+  }
+
+  .navbar a {
+    color: white;
+    text-decoration: none;
+    font-size: 18px;
+    padding: 8px;
+    border-radius: 4px;
+  }
+
+  .navbar .logo {
+    max-width: 150px;
+  }
+
+  /* Responsive styling */
+  @media (max-width: 768px) {
+    .navbar {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	// import { i18n } from '$lib/i18n';
+	// import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
-	let { children } = $props();
+	import Navbar from '$lib/components/Navbar.svelte';
+	// let { children } = $props();
 </script>
 
-<ParaglideJS {i18n}>
+<!-- <ParaglideJS {i18n}>
 	{@render children()}
-</ParaglideJS>
+</ParaglideJS> -->
+
+<Navbar /> <!-- Add the Navbar component here -->
+
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    padding: 20px;
+  }
+</style>
